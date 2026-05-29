d20521d4cacc43255274439a072f414939263dcb
3zden <azeddineabouaam1@gmail.com>
2026-05-29 23:21:24 +0100

    update : remove unnecessary imports in Security Config Class

--- src/main/java/com/_zden/BloggingApp/Configuration/SecurityConfig.java
+++ src/main/java/com/_zden/BloggingApp/Configuration/SecurityConfig.java
@@ -6,12 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AnyRequestMatcher;
-import org.springframework.util.AntPathMatcher;
-//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.util.PathMatcher;
 
-import static org.springframework.web.servlet.function.RequestPredicates.headers;
 
 @Configuration
 @EnableWebSecurity
