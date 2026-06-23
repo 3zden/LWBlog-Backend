311384f720ac6a1c027424c8fdbf4b2193543872
3zden <azeddineabouaam1@gmail.com>
2026-06-23 22:29:39 +0100

    fix : fix securitty config

--- src/main/java/com/_zden/BloggingApp/Configuration/SecurityConfig.java
+++ src/main/java/com/_zden/BloggingApp/Configuration/SecurityConfig.java
@@ -1,29 +1,25 @@
 package com._zden.BloggingApp.Configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.security.autoconfigure.web.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-// Make sure you're importing THIS:
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
     UserDetailsService userDetailsService;
@@ -45,7 +41,7 @@ public class SecurityConfig {
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
         return provider;
     }
 }
